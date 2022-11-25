41b4f4f99bdc4cda90b684e65ec45409ef4ef8ae
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-11-25 14:29:15 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,7 +4,6 @@ echo starting server
 # here we just use npm to run the build
 cd /var/www/
 echo building application...
-sudo npm run build
 
 # start the application with pm2
 echo starting application...
